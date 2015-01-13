56e6f14dafd9fe226547daa8deac2c21036aa9d5
tarzelus <tarzeluset14dw@ikzubirimanteo.com>
2015-01-13 09:02:01 +0100

    iglesia gehituta

---
+++ EspacioPublico.java
@@ -0,0 +1,60 @@
+//package com.zubiri.elecciones;
+import java.util.Scanner;
+
+
+public class EspacioPublico {
+
+
+
+
+		
+		private String Direccion;
+		private String Tipo;
+		private String Localidad;
+	
+		
+//--------------------direccion--------------------------------	
+		public String getDireccion()
+		{
+			return Direccion;
+		}
+		
+		public void  setDireccion(String direccion1)
+		{
+			Direccion = direccion1;
+		}
+
+
+//--------------------tipo--------------------------------
+
+		public String getTipo()
+		{
+			return Tipo;
+		}
+		
+		public void  setTipo(String tipo1)
+		{
+			Tipo=tipo1;
+		}
+
+
+//---------------------localidad-------------------------------
+
+		public String getLocalidad()
+		{
+			return Localidad;
+		}
+		
+		public void  setLocalidad(String localidad1)
+		{
+	 		Localidad = localidad1;
+		}
+
+
+
+
+
+	
+
+	
+}
---
+++ Iglesias.java
@@ -0,0 +1,47 @@
+public class Iglesias extends EspacioPublico
+{
+	private String nombreIglesia;
+	private int idIglesia;
+	private String tipoIglesia;
+
+        public Iglesias() {
+		super();
+        }
+
+	// getter y setter
+
+		public String getNombreiglesia()
+		{
+			return nombreIglesia;
+		}
+		
+		public void  setNombreIglesia(String nombreIglesia1)
+		{
+			nombreIglesia=nombreIglesia1;
+		}
+		
+		
+		
+		public int getIdIglesia()
+		{
+			return idIglesia;
+		}
+		
+		public void  setIdIglesia(int idIglesia1)
+		{
+			idIglesia=idIglesia1;
+		}
+
+		
+		
+		public String getTipoIglesia()
+		{
+			return tipoIglesia;
+		}
+		
+		public void  setTipoIglesia(String tipoIglesia1)
+		{
+			tipoIglesia=tipoIglesia1;
+		}
+	
+}
